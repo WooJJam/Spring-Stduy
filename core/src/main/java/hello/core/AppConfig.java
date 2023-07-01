3b4fb5f35aa72a7097820e7f6fe4810c1224b683
우재민 <woojjam@ujaemin-ui-MacBookAir.local>
2023-07-02 01:11:01 +0900

    [SECTION 6] @Configuration과 바이트코드 조작

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -17,19 +17,24 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
     // Bean : Spring 컨테이너에 등록됨
+    // @Bean memberService -> new MemoryMemberRepository()
+    // @Bean orderService -> new MemoryMemberRepository()
     @Bean
     // 생성자 주입
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
-    public static MemberRepository memberRepository() {
+    public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
